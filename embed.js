2d838301cdea8b55b0a6cda47bf42d977ca782cd
Kanishk Gupta <kanishkgupta1234@gmail.com>
2020-06-19 20:06:42 +0530

    Added files

---
+++ embed.js
@@ -0,0 +1,12 @@
+var script = document.getElementById('emb')
+var data= script.getAttribute("data");
+console.log(data+" hui");
+document.head.insertAdjacentHTML( 'beforeend', '<link rel=stylesheet href=/ext.css>' );
+document.write(`
+<div class="card">
+<div id="dimg"><img src="${data}/favicon.ico" id="uimg"/></div>
+<div class="container">
+<a href=${data} target="_blank" rel="noreferrer noopener" class=button1>Visit</a>
+<h4>${data}</h4>
+</div>
+</div>`);
